91ba01a9f193d8b70784ae1057ea43fb48f9a219
Sagar Toms <sagartoms@gmail.com>
2018-06-29 00:05:17 -0400

    demo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {}
 
 config.host = process.env.HOST || "https://cosmosdbcm.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "qncHJhnkoDABuC9fxUMxrpESJVJyMofMjgLxqVKEILarjQTCo0Y6n4PxW4SSh5UExX7NKJmlGPYbEs7Ue8xwiA==";
+config.authKey = process.env.AUTH_KEY || "KdfN8UjpGmmE7W2he25t1U8SvuxsZ1rvmDgLiRGEkwRavnBYEgcNkVi1x14W3tFjgfgWfnZrc84zz2BVeRlBUA==";
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
 
